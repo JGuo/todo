a68cb8062749546c4733819bed62649fb9b748c8
JGuo <jisiguo@Jisi-MacBook.local>
2016-03-09 20:17:57 -0800

    testing a merge conflict

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print ("i don't pity the foo anymore!")
+        print ("i don't pity pooop the foo anymore!")
     }
 
     override func didReceiveMemoryWarning() {
