7b134efdc1ec85544ee0781feb18e0da001adb89
Vishay Nihalani <vnihalani@lyft.com>
2016-03-09 20:42:25 -0800

    initial todoviewcontroller
    includes a basic tableviewcontroller with cells that change colors
    based on how many todo items there are

---
+++ todo/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  todo
+//
+//  Created by Vishay Nihalani on 3/9/16.
+//  Copyright © 2016 Jisi Guo. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ todo/ToDoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Jisi Guo on 2/29/16.
+//  Copyright © 2016 Jisi Guo. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todos : NSArray!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        todos = [0, 1, 2, 3, 4, 5, 6, 7]
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.todo.todocellview", forIndexPath: indexPath) as! ToDoTableViewCell
+        
+        let multiplier = 255.0 / CGFloat(todos.count)
+        
+        cell.backgroundColor = UIColor(red: CGFloat(indexPath.row) * multiplier / 255.0, green: CGFloat(indexPath.row) * multiplier / 255.0, blue: CGFloat(indexPath.row) * multiplier / 255.0, alpha: 1.0)
+
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return todos.count
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
