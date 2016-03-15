e7b931cf5bffeac7bd33c39da6150ead3e1633ad
Vishay Nihalani <vnihalani@lyft.com>
2016-03-14 18:05:59 -0700

    Swipe to create new cell
    * made editable text labels in each cell
    * swipe down creates a new cell
    todo: need to save the cell

--- todo/ToDoViewController.swift
+++ todo/ToDoViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let ROW_HEIGHT = CGFloat(70)
+
 class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var scrollView: UIScrollView!
@@ -15,32 +17,27 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     
     var todos : NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         scrollView.contentSize = CGSize(width: 3 * 375, height: self.view.frame.height)
         
-        todos = [   "0",
-                    "1",
-                    "2",
-                    "3",
-                    "4",
-                    "5",
-                    "6",
-                    "7",
-                    "8",
-                    "9",
-                    "10",
-                    "11",
-                    "12",
-                    "13",
-                    "14",
-                    "15",
-                    "16",
-                    "17",
-                    "18",
-                    "19"]
+        todos = [   "Learn",
+                    "Coffee",
+                    "Ideation",
+                    "Coffee",
+                    "Wireframe",
+                    "Coffee",
+                    "Code",
+                    "Coffee",
+                    "Submit",
+                    "Coffee",
+                ]
+    
+        // Hide the new cell, so that it's only revealed on scroll
+        tableView.contentInset.top = -ROW_HEIGHT
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -51,7 +48,7 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         cell.backgroundColor = UIColor(red: CGFloat(indexPath.row + 5) * multiplier / 255.0, green: 0, blue: 0, alpha: 1.0)
         
-        cell.textLabel?.text = todos[indexPath.row] as! String
+        cell.textField.text = todos[indexPath.row] as! String
 
         return cell
     }
@@ -60,6 +57,29 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         return todos.count
     }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return ROW_HEIGHT
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+     
+        // When you scroll to the top of the scroll view, catch the New cell and leave it on screen
+        if (tableView.contentOffset.y <= 0) {
+            
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                
+                self.tableView.contentInset.top = 0
+                },  completion: { (finished) -> Void in
+              
+                    let cell = self.tableView.cellForRowAtIndexPath(NSIndexPath(forItem: 0, inSection: 0)) as! ToDoTableViewCell
+                    
+                    cell.textField!.becomeFirstResponder()
+            })
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
